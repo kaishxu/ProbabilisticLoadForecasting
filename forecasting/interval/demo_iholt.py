a995facd7903816880fb71c62a08494f6b27b98f
Kaishuai <xu.kaishuai@gmail.com>
2019-12-24 10:28:28 +0800

    iholt

--- forecasting/interval/demo_iholt.py
+++ forecasting/interval/demo_iholt.py
@@ -9,7 +9,7 @@ months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 methods = ['hierarchical/euclidean', 'hierarchical/cityblock', 'hierarchical/hausdorff', 'kmeans']
 data_sets = ['Irish_2010', 'London_2013']
 
-path = os.path.abspath(os.path.join(os.getcwd(), '../..'))
+path = os.path.abspath(os.path.join(os.getcwd()))
 
 for data_set in data_sets:
 
@@ -28,16 +28,23 @@ for data_set in data_sets:
                 path_cluster = os.path.join(path, 'result', data_set, 'clustering', 'interval', method, f'n_clusters_{n_clusters}.csv')
                 clusters = pd.read_csv(path_cluster, header=None)
                 series = data[:, (month-1)*2:month*2, :months[month-1]*24]
-                print('Start train...', 'Month: ', month, 'Series shape: ', series.shape)
 
                 total_pred_series = []
                 total_xs = []
+                total_scale = []
                 for i in range(n_clusters):
                     index = list(clusters[month-1] == i)
                     sub_series = series[index]
                     sub_series = np.sum(sub_series, axis=0)
                     test = sub_series[:, -168:]
                     train = sub_series[:, :-168]
+
+                    scale = np.zeros(2)
+                    scale[0] = np.max(train)
+                    scale[1] = np.min(train)
+                    total_scale.append(scale)
+                    train = (train - scale[1])/(scale[0] - scale[1])
+                    test = (test - scale[1])/(scale[0] - scale[1])
                     
                     # test window (h = 1, 2, ..., 7)
                     pred_series = []
@@ -62,8 +69,10 @@ for data_set in data_sets:
 
                 total_pred_series = np.array(total_pred_series)
                 total_xs = np.array(total_xs)
+                total_scale = np.array(total_scale)
                 path_result = os.path.join(path, 'result', data_set, 'forecasting', 'interval', method)
                 np.save(os.path.join(path_result, f'n_clusters_{n_clusters}_month_{month}.npy'), total_pred_series)
                 np.save(os.path.join(path_result, f'n_clusters_{n_clusters}_month_{month}_params.npy'), total_xs)
+                np.save(os.path.join(path_result, f'n_clusters_{n_clusters}_month_{month}_scale.npy'), total_scale)
 
-                print('Finish train...', 'data_set: ', data_set, 'method: ', method, 'n_clusters: ', n_clusters, 'month: ', month)
+                print('data_set:', data_set, ', method:', method, ', n_clusters:', n_clusters, ', month:', month, ', series shape:', series.shape)
